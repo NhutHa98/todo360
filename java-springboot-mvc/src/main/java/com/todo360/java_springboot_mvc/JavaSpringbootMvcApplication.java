ef3c5d1d59df9c86eb8830d186bed097381d4559
LeCua <ultrav8800@gmail.com>
2025-09-03 23:09:05 +0700

    Initialize Java Spring Boot MVC project with Maven wrapper, configuration files, and basic application structure

---
+++ java-springboot-mvc/src/main/java/com/todo360/java_springboot_mvc/JavaSpringbootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.todo360.java_springboot_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootMvcApplication.class, args);
+	}
+
+}
---
+++ java-springboot-mvc/src/test/java/com/todo360/java_springboot_mvc/JavaSpringbootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo360.java_springboot_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
